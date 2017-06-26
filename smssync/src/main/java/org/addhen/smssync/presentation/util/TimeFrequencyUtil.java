b901742bed309f8926310319a18198b09a6180eb
Rok Mohar <rok.mohar@gmail.com>
2017-06-26 17:59:17 +0200

    Customize SMSsync.

--- smssync/src/main/java/org/addhen/smssync/presentation/util/TimeFrequencyUtil.java
+++ smssync/src/main/java/org/addhen/smssync/presentation/util/TimeFrequencyUtil.java
@@ -25,15 +25,15 @@ public class TimeFrequencyUtil {
     public static final String DEFAULT_TIME_FREQUENCY = "00:05";
 
     private static final int ONE_HOUR = 3600000;
-
     private static final int ONE_MINUTE = 60000;
+    private static final int ONE_SECOND = 1000;
 
     private TimeFrequencyUtil() {
     }
 
     public static long calculateInterval(String time) {
         String[] pieces = time.split(":");
-        return Integer.parseInt(pieces[0]) * ONE_HOUR +
-                Integer.parseInt(pieces[1]) * ONE_MINUTE;
+        return Integer.parseInt(pieces[0]) * ONE_MINUTE +
+                Integer.parseInt(pieces[1]) * ONE_SECOND;
     }
 }
